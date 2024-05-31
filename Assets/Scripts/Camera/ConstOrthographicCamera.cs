e4d05b7f5edff7e4149a6e6d24bed437ffc8fdee
Kudrman <kudrman_93@mail.ru>
2024-05-31 20:23:35 +0700

    добавление плоскости, камеры, поведения камеры

---
+++ Assets/Scripts/Camera/ConstOrthographicCamera.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ConstOrthographicCamera : MonoBehaviour
+{
+    [SerializeField] private Vector2 _defaultResolution = new Vector2(1600, 900);
+
+    private Camera _camera;
+
+    private float _initialSize;
+    private float _targetAspect;
+    private float _prevCameraAspect;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _camera = GetComponent<Camera>();
+
+        _initialSize = _camera.orthographicSize;
+        _targetAspect = _defaultResolution.x / _defaultResolution.y;
+        _prevCameraAspect = _camera.aspect;
+    }
+
+    private void Update()
+    {
+        RefreshCamera();
+    }
+
+    private void RefreshCamera()
+    {
+        if(Mathf.Approximately(_camera.aspect, _prevCameraAspect))
+        {
+            return;
+        }
+        _prevCameraAspect = _camera.aspect;
+
+        float constantWidthSize = _initialSize * (_targetAspect / _camera.aspect);
+        _camera.orthographicSize = Mathf.Max(constantWidthSize, _initialSize);
+    }
+}
